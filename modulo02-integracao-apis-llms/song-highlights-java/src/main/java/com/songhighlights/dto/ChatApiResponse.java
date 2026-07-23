e2275001e6c95ffc1bc927d793076759b9335752
wasilva <vvagner.silva@gmail.com>
2026-07-23 11:00:20 -0300

    Sincronizacao de projetos

--- modulo02-integracao-apis-llms/song-highlights-java/src/main/java/com/songhighlights/dto/ChatApiResponse.java
+++
@@ -1,9 +0,0 @@
-package com.songhighlights.dto;
-
-public record ChatApiResponse(
-        String userId,
-        String reply,
-        boolean preferencesUpdated,
-        boolean conversationSummarized
-) {
-}
--- modulo02-integracao-apis-llms/song-highlights-java/src/main/java/com/songhighlights/service/ChatTurnResult.java
+++
@@ -1,9 +0,0 @@
-package com.songhighlights.service;
-
-public record ChatTurnResult(
-        String userId,
-        String reply,
-        boolean preferencesUpdated,
-        boolean conversationSummarized
-) {
-}
